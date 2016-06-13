905fcd99d85048d7932aaa30bd6a74905e17f830
techiesms <sachinsms1990@gmail.com>
2016-06-13 11:11:03 +0530

    Over The Air Programming
    This is the code for OTA programming in you ESP8266 12E. With this you
    can upload your wirelessly via WiFi.

---
+++ OTA.ino
@@ -0,0 +1,151 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+const char* ssid = "SSID Name";
+const char* password = "Password";
+int led = 16;
+int flash = 0;
+
+
+/************************* Adafruit.io Setup *********************************/
+
+#define AIO_SERVER      "io.adafruit.com"
+#define AIO_SERVERPORT  1883                   // use 8883 for SSL
+#define AIO_USERNAME    "username"
+#define AIO_KEY         "KEY"
+
+
+bool flag =0; // For selecting Mode
+
+WiFiClient client;
+
+const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
+const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
+const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
+
+// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, AIO_SERVERPORT, MQTT_USERNAME, MQTT_PASSWORD);
+
+
+
+const char Relay2[] PROGMEM = AIO_USERNAME "/feeds/Mode";
+Adafruit_MQTT_Subscribe Mode = Adafruit_MQTT_Subscribe(&mqtt, Relay2);
+/*************************** Sketch Code ************************************/
+
+// Bug workaround for Arduino 1.6.6, it seems to need a function declaration
+// for some reason (only affects ESP8266, likely an arduino-builder bug).
+void MQTT_connect();
+
+
+void setup() {
+  pinMode(led,OUTPUT);
+  pinMode(flash,INPUT);
+
+  pinMode(5,OUTPUT);
+  Serial.begin(115200);
+  Serial.println("Booting");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  // Port defaults to 8266
+  // ArduinoOTA.setPort(8266);
+
+  // Hostname defaults to esp8266-[ChipID]
+  // ArduinoOTA.setHostname("My ESP");
+
+  // No authentication by default
+  // ArduinoOTA.setPassword((const char *)"123"); // password
+
+  ArduinoOTA.onStart([]() {
+    digitalWrite(led,HIGH);
+    Serial.println("Start");
+  });
+  ArduinoOTA.onEnd([]() {
+    digitalWrite(led,LOW);
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  mqtt.subscribe(&Mode);
+}
+
+void loop() {
+
+  MQTT_connect();
+
+Adafruit_MQTT_Subscribe *subscription;
+  while ((subscription = mqtt.readSubscription(10))) {
+   
+    if (subscription == &Mode) {
+      Serial.print(F("Got_Mode: "));
+      Serial.println((char *)Mode.lastread);
+      uint16_t num = atoi((char *)Mode.lastread);
+      flag= num;
+    }
+  }
+
+  
+  if(flag==1) // OTA mode
+  ArduinoOTA.handle();
+  
+  else // normal mode
+  {
+    digitalWrite(led,LOW);
+    delay(50);
+    digitalWrite(led,HIGH);
+    delay(30);
+    
+  }
+  
+
+}
+
+
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 5 seconds...");
+       mqtt.disconnect();
+       delay(5000);  // wait 5 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
